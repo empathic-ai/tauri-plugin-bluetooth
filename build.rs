2784ea092f029189bb9fc2d63117686e4633b7f2
Catchawink <threeeasywords@gmail.com>
2024-04-20 14:33:15 -0400

    Updated files.

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ const COMMANDS: &[&str] = &["ping"];
 fn main() {
   #[cfg(not(target_arch = "wasm32"))]
   tauri_plugin::Builder::new(COMMANDS)
+    //.global_api_script_path("./api-iife.js")
     .android_path("android")
     .ios_path("ios")
     .build();
