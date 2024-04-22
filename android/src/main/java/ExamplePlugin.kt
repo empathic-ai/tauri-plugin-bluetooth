102433faa6fc71b6e6cf20b33c583eb27c32a8b0
Catchawink <threeeasywords@gmail.com>
2024-04-21 20:38:14 -0400

    Updated files.

--- android/src/main/java/ExamplePlugin.kt
+++ android/src/main/java/ExamplePlugin.kt
@@ -8,6 +8,12 @@ import app.tauri.plugin.JSObject
 import app.tauri.plugin.Plugin
 import app.tauri.plugin.Invoke
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
 @InvokeArg
 class PingArgs {
   var value: String? = null
@@ -17,12 +23,56 @@ class PingArgs {
 class ExamplePlugin(private val activity: Activity): Plugin(activity) {
     private val implementation = Example()
 
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                // Add the device to a list or display its name and address
+            }
+        }
+    }
+
     @Command
     fun ping(invoke: Invoke) {
+      
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show()
+            activity.finish()
+        }
+
+        if (!bluetoothAdapter.isEnabled) {
+          val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+          startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT)
+        }
+
+        companion object {
+            private const val REQUEST_ENABLE_BT = 1
+        }
+
+        //val discoverDevicesIntent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        //registerReceiver(receiver, discoverDevicesIntent)
+        //bluetoothAdapter.startDiscovery()
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice d: pairedDevices) {
+                String deviceName = d.getName();
+                String macAddress = d.getAddress();
+                Log.i(LOGTAG, "paired device: " + deviceName + " at " + macAddress);
+                // do what you need/want this these list items
+            }
+        }
+
         val args = invoke.parseArgs(PingArgs::class.java)
 
         val ret = JSObject()
         ret.put("value", implementation.pong(args.value ?: "default value :("))
         invoke.resolve(ret)
     }
+    
 }
