35325bffde0347709b3e202f091f037509e2c1e5
Catchawink <threeeasywords@gmail.com>
2024-05-06 09:13:57 -0400

    Updated files.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -42,5 +42,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(":tauri-android"))
-    api(project(":btleplug"))
+    implementation(project(":btleplug"))
 }
