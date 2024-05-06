0f46ebe27847d3ccbf86687cf6c6a2e634f55b75
Catchawink <threeeasywords@gmail.com>
2024-05-06 10:42:11 -0400

    Consolidated project.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -42,5 +42,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(":tauri-android"))
-    implementation(project(":btleplug"))
 }
