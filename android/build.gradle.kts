6355a68a70ac68eb624299b4c304c952b5ddbe51
Catchawink <threeeasywords@gmail.com>
2024-05-06 10:49:00 -0400

    Updated dependencies.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -33,7 +33,6 @@ android {
     }
 }
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
@@ -42,4 +41,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(":tauri-android"))
+    implementation("io.github.gedgygedgy.rust:jni-utils:0.1.1-SNAPSHOT")
 }
