3601242e23f44193bc5f9a121059076f6480b62a
Catchawink <threeeasywords@gmail.com>
2024-05-06 21:45:04 -0400

    Updated files.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -58,5 +58,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(":tauri-android"))
-    implementation("io.github.gedgygedgy.rust:jni-utils:0.1.1-SNAPSHOT")
+    implementation(":jni-utils")
 }
